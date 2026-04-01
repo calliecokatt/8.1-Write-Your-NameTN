5d6b314babed1172dff91c5dca5088d7bc4c0f27
calliecokatt <zeebyfleeby@gmail.com>
2026-04-01 16:31:12 -0500

    basic window setup and some turtle controls

---
+++ main.py
@@ -0,0 +1,41 @@
+# meow! meow! meow! :3
+from turtle import *
+# screen stuff meow
+screen = Screen()
+screen.bgcolor("black")
+screen.title("name project meow")
+screen.setup(width=1000, height=400)
+# turtle setup time! meow
+turt = Turtle("turtle")
+turt.color("purple")
+# tutle move time! meow
+
+turt.penup
+turt.goto(-300,-100)
+turt.goto(-300,0)
+turt.goto(-500,-100)
+turt.goto(-100,-100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# weird exit on click thing. i dont know why we need it but it's here ig......
+screen.exitonclick()
+0
